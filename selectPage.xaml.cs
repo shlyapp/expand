3f8c20929d71342373039f446bc697ac084d787e
shlyapp <78727203+shlyapp@users.noreply.github.com>
2021-03-21 16:53:02 +0400

    transferring arrays to selectPage

--- selectPage.xaml.cs
+++ selectPage.xaml.cs
@@ -22,7 +22,22 @@ namespace expand
     {
         string nextpage;
 
-        string 
+        // source files
+
+        string[] photoFilesAnimals = { "image/animals/cat.jpg", "image/animals/cow.jpg", "image/animals/dog.jpg", "image/animals/elephant.jpg", "image/animals/fish.jpg",
+        "image/animals/hamster.jpg", "image/animals/horse.jpg", "image/animals/mouse.jpg", "image/animals/pig.jpg", "image/animals/sheep.jpg", "image/animals/wolf.jpg"};
+        string[] namesAnimals = { "cat", "cow", "dog", "elephant", "fish", "hamster", "horse", "mouse", "pig", "sheep", "wolf"};
+
+        string[] photoFilesFruit = { "image/apple.jpg", "image/banana.jpg", "image/fruit.jpg", "image/kivi.jpg", "image/pineapple.jpg", "image/melon.jpg", "image/tomato.jpg",
+        "image/pumpkin.jpg", "image/pea.jpg", "image/lime.jpg", "image/corn.jpg", "image/cherry.jpg", "image/carrot.jpg" };
+        string[] namesFruit = { "apple", "banana", "fruit", "kiwi", "pineapple", "melon", "tomato", "pumpkin", "pea", "lime", "corn", "cherry", "carrot" };
+
+        string[] photoFilesSchool = { };
+        string[] namesSchool = { };
+
+        string[] photoFilesMusic = { };
+        string[] namesMusic = { };
+
         public selectPage(string nextPage)
         {
             InitializeComponent();
@@ -32,7 +47,7 @@ namespace expand
         private void animals_Click(object sender, RoutedEventArgs e)
         {
             if (nextpage == "cards") NavigationService.Navigate(new cardsPage("animals"));
-            if (nextpage == "translate") NavigationService.Navigate(new translatePage("animals"));
+            if (nextpage == "translate") NavigationService.Navigate(new translatePage(photoFilesAnimals, namesAnimals));
             if (nextpage == "trueOrFalse") NavigationService.Navigate(new trueOrFalsePage("animals"));
             if (nextpage == "putACouple") NavigationService.Navigate(new putACouplePage("animals"));
         }
@@ -40,7 +55,7 @@ namespace expand
         private void fruit_Click(object sender, RoutedEventArgs e)
         {
             if (nextpage == "cards") NavigationService.Navigate(new cardsPage("fruit"));
-            if (nextpage == "translate") NavigationService.Navigate(new translatePage("fruit"));
+            if (nextpage == "translate") NavigationService.Navigate(new translatePage(photoFilesFruit, namesFruit));
             if (nextpage == "trueOrFalse") NavigationService.Navigate(new trueOrFalsePage("fruit"));
             if (nextpage == "putACouple") NavigationService.Navigate(new putACouplePage("fruit"));
         }
@@ -48,7 +63,7 @@ namespace expand
         private void school_Click(object sender, RoutedEventArgs e)
         {
             if (nextpage == "cards") NavigationService.Navigate(new cardsPage("school"));
-            if (nextpage == "translate") NavigationService.Navigate(new translatePage("school"));
+            if (nextpage == "translate") NavigationService.Navigate(new translatePage(photoFilesSchool, namesSchool));
             if (nextpage == "trueOrFalse") NavigationService.Navigate(new trueOrFalsePage("school"));
             if (nextpage == "putACouple") NavigationService.Navigate(new putACouplePage("school"));
         }
@@ -56,7 +71,7 @@ namespace expand
         private void music_Click(object sender, RoutedEventArgs e)
         {
             if (nextpage == "cards") NavigationService.Navigate(new cardsPage("music"));
-            if (nextpage == "translate") NavigationService.Navigate(new translatePage("music"));
+            if (nextpage == "translate") NavigationService.Navigate(new translatePage(photoFilesMusic, namesMusic));
             if (nextpage == "trueOrFalse") NavigationService.Navigate(new trueOrFalsePage("music"));
             if (nextpage == "putACouple") NavigationService.Navigate(new putACouplePage("music"));
         }
--- translatePage.xaml.cs
+++ translatePage.xaml.cs
@@ -20,55 +20,36 @@ namespace expand
     {
         int i = 0;
         //public string Theme { get; set; }
-        public string[] Value { get; set; }
-        public string[] Source { get; set; }
-        public translatePage(string theme)
+        public string[] Photo { get; set; }
+        public string[] Names { get; set; }
+        public translatePage(string[] photo, string[] names)
         {
             
             InitializeComponent();
             //this.Theme = theme;
+            this.Photo = photo;
+            this.Names = names;
+           
 
-            if (theme == "fruit")
-            {
-                this.Source = new string[] {"image/fruit/apple.jpg", "image/fruit/banana.jpg", "image/fruit/fruit.jpg", "image/fruit/kivi.jpg", "image/fruit/pineapple.jpg", "image/fruit/melon.jpg", "image/fruit/tomato.jpg",
-                             "image/fruit/pumpkin.jpg", "image/fruit/pea.jpg", "image/fruit/lime.jpg", "image/fruit/corn.jpg", "image/fruit/cherry.jpg", "image/fruit/carrot.jpg"};
-                this.Value = new string[] { "apple", "banana", "fruit", "kiwi", "pineapple", "melon", "tomato", "pumpkin", "pea", "lime", "corn", "cherry", "carrot" };
-            }
-            else if (theme == "animals")
-            {
-                this.Source = new string[] { };
-                this.Value = new string[] { };
-            }
-            else if (theme == "school")
-            {
-                this.Source = new string[] { };
-                this.Value = new string[] { };
-            }
-            else if (theme == "music")
-            {
-                this.Source = new string[] { };
-                this.Value = new string[] { };
-            }
-
-            image.Source = new BitmapImage(new Uri(Source[i], UriKind.Relative));
+            image.Source = new BitmapImage(new Uri(Photo[i], UriKind.Relative));
         }
        
         private void TextBox_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.Enter)
             {
-                if (Answer.Text.Trim() == Value[i])
+                if (Answer.Text.Trim() == Names[i])
                 {
                     MessageBox.Show("All right!");
                 }
                 else
                 {
-                    string rightanswer = "No!\nRight answer: " + Value[i];
+                    string rightanswer = "No!\nRight answer: " + Names[i];
                     MessageBox.Show(rightanswer);
                 }
                 i++;
                 Answer.Clear();
-                if (i != Value.Length - 1) image.Source = new BitmapImage(new Uri(Source[i], UriKind.Relative));
+                if (i != Names.Length - 1) image.Source = new BitmapImage(new Uri(Photo[i], UriKind.Relative));
                 else
                 {
                     MessageBox.Show("Все!");
